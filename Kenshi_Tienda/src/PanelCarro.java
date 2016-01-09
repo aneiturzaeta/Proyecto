3585a0cff4bd95f325108277029cbaf68f34f298
Ane <Alumno@192.168.0.160>
2016-01-09 23:10:43 +0100

    Ane Iturzaeta
    Vuelvo a subir PanelCarro y bd, porque daba error. Error solucionado.

--- Kenshi_Tienda/src/PanelCarro.java
+++ Kenshi_Tienda/src/PanelCarro.java
@@ -23,7 +23,7 @@ public class PanelCarro extends JPanel implements ActionListener{
 	private JButton bBorrar;
 	
 	 
-	private Cliente sesion;
+	private Cliente sesion = new Cliente("", "", "", "", "");
 	
 	
 	public PanelCarro (){
@@ -35,78 +35,80 @@ public class PanelCarro extends JPanel implements ActionListener{
 			
 			//rellenar la tabla. 1) buscar en BD. 2) Imprimir los datos
 			
+			
+			
 			ArrayList<Pedido> pedidos = new ArrayList<Pedido>();
 			
 			
-			if (sesion.getNombreusuario().equals("")){
-				
-				
-				JOptionPane.showMessageDialog(this, "Primero inicia sesion. No hay cesta que mostrar");	
-				
-			}
-			
-			else {
-			
-			
-			pedidos = BaseDeDatos.buscarPedidos(sesion.getNombreusuario());
-			
-					
-			String[] columnas = {"Codigo Producto","Fecha"};
-			
-			
-			//se cargan desde la BD. 
-			
-			
-			for ( Pedido pedido : pedidos){
-				   
-				String codigo = pedido.getCodigo();
-				String fecha = pedido.getFecha();
-				   
-				//hay que ir metiendo fila por fila todas las entradas. 1 entrada = 1 pedido
-				
-//				Object[][] datos = {codigo, fecha};
-
-//				table = new JTable(datos, columnas);
-
-				}
-			
-			
-			
-			
-			
-			
-			
-//			datos.add("01", "ww");
-			
-			
-			
-			table.setBounds(49, 65, 298, 295);
-			add(table);
-			
-			
-						
-			
-			textComplemento = new JLabel();
-			textComplemento.setFont(new Font("Century Gothic", Font.BOLD, 12));
-			textComplemento.setText("Estos son los productos que tienes en tu carro");
-			textComplemento.setOpaque(false);
-			textComplemento.setForeground(Color.BLACK);
-			textComplemento.setBounds(60, 23, 280, 20);
-			add(textComplemento);
-			
-			bBorrar = new JButton("Borrar todos los pedidos");
-			bBorrar.setSize(250, 32);
-			bBorrar.setLocation(70, 380);
-			bBorrar.setFont(new Font("Century Gothic", Font.BOLD, 16));
-			bBorrar.addActionListener(this);
-			bBorrar.setActionCommand("Borrar");
-			bBorrar.setContentAreaFilled(false);
-			bBorrar.setBorderPainted(false);
-			add(bBorrar);
-	
-			
-			
-			}
+//			if (sesion.getNombreusuario().equals("")){
+//				
+//				
+//				JOptionPane.showMessageDialog(this, "Primero inicia sesion. No hay cesta que mostrar");	
+//				
+//			}
+//			
+//			else {
+//			
+//			
+////			pedidos = BaseDeDatos.buscarPedidos(sesion.getNombreusuario());
+//			
+//					
+//			String[] columnas = {"Codigo Producto","Fecha"};
+//			
+//			
+//			//se cargan desde la BD. 
+//			
+//			
+//			for ( Pedido pedido : pedidos){
+//				   
+//				String codigo = pedido.getCodigo();
+//				String fecha = pedido.getFecha();
+//				   
+//				//hay que ir metiendo fila por fila todas las entradas. 1 entrada = 1 pedido
+//				
+////				Object[][] datos = {codigo, fecha};
+//
+////				table = new JTable(datos, columnas);
+//
+//				}
+//			
+//			
+//			
+//			
+//			
+//			
+//			
+////			datos.add("01", "ww");
+//			
+//			
+//			
+//			table.setBounds(49, 65, 298, 295);
+//			add(table);
+//			
+//			
+//						
+//			
+//			textComplemento = new JLabel();
+//			textComplemento.setFont(new Font("Century Gothic", Font.BOLD, 12));
+//			textComplemento.setText("Estos son los productos que tienes en tu carro");
+//			textComplemento.setOpaque(false);
+//			textComplemento.setForeground(Color.BLACK);
+//			textComplemento.setBounds(60, 23, 280, 20);
+//			add(textComplemento);
+//			
+//			bBorrar = new JButton("Borrar todos los pedidos");
+//			bBorrar.setSize(250, 32);
+//			bBorrar.setLocation(70, 380);
+//			bBorrar.setFont(new Font("Century Gothic", Font.BOLD, 16));
+//			bBorrar.addActionListener(this);
+//			bBorrar.setActionCommand("Borrar");
+//			bBorrar.setContentAreaFilled(false);
+//			bBorrar.setBorderPainted(false);
+//			add(bBorrar);
+//	
+//			
+//			
+//			}
 			
 			
 			
